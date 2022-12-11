af6ee7376f53f42df3273464e3c88acfbec46460
Niel Mistry <niel@nielmistry.com>
2022-12-11 01:03:15 -0500

    day3!

--- 2022/rust/day3/src/lib.rs
+++ 2022/rust/day3/src/lib.rs
@@ -1,27 +1,54 @@
+#![feature(iter_array_chunks)]
+
 use std::collections::HashSet;
 use std::io::{stdout, Write};
 
+fn get_char_priority(character: char) -> u32 {
+    let char_code = character as u32;
+    let output = match character {
+        'a'..='z' => 1 + (char_code - 'a' as u32),
+        'A'..='Z' => 27 + (char_code - 'A' as u32),
+        _ => panic!("Not a good character!"),
+    };
+    output
+}
+
 pub fn part1(input: &String) -> u32 {
-    println!("Hello");
-    let rucksacks: u8 = input.split("\n").enumerate().map(|x| '0' as u8).sum();
-    println!("{}", rucksacks);
+    let v = input
+        .split("\n")
+        .map(|x| {
+            let len = x.len();
+            let part1 = &x[0..len / 2];
+            let part2 = &x[(len / 2)..];
 
-    // for rucksack in rucksacks {
-    //     let rucksack_len = rucksack.len();
-    //     let part_a = &rucksack[0..rucksack_len / 2];
-    //     let part_b = &rucksack[(rucksack_len / 2) + 1..];
+            let entries: HashSet<char> = part1.chars().collect();
+            for c in part2.chars() {
+                if entries.contains(&c) {
+                    return c;
+                }
+            }
 
-    //     println!("{} | {}", part_a, part_b);
+            panic!("Shouldn't have got here!");
+        })
+        .map(|character| get_char_priority(character))
+        .sum();
 
-    //     let entries: HashSet<char> = part_a.chars().collect();
-    //     for c in part_b.chars() {
-    //         if entries.contains(&c) {
-    //             println!("{} -> {}", c, c as u8);
-    //             break;
-    //         }
-    //     }
-    // }
-    0
+    v
+}
+pub fn part2(input: &String) -> u32 {
+    let v = input
+        .lines()
+        .array_chunks::<3>()
+        .map(|[a, b, c]| {
+            let citem = a
+                .chars()
+                .find(|char_in_a| b.contains(*char_in_a) && c.contains(*char_in_a));
+            citem
+        })
+        .map(|character| get_char_priority(character.unwrap()))
+        .sum();
+
+    v
 }
 
 #[cfg(test)]
@@ -38,5 +65,11 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw";
         let ans = part1(&input.to_string());
         assert_eq!(157, ans);
+    }
+    #[test]
+    fn part1_test2() {
+        let input = "abbd";
+        let ans = part1(&input.to_string());
+        assert_eq!(2, ans);
     }
 }
