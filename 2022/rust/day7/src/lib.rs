3fed645e2465e1394937536cc7a92e0fcb954767
Niel Mistry <niel@nielmistry.com>
2022-12-29 12:33:30 -0500

    hashmap based tree better!

--- 2022/rust/day7/src/lib.rs
+++ 2022/rust/day7/src/lib.rs
@@ -1,30 +1,34 @@
-use rand::Rng;
-use std::collections::HashMap;
+use std::{
+    collections::{hash_map::DefaultHasher, HashMap},
+    hash::{Hash, Hasher},
+};
 
 enum ContentType {
-    Folder(u32),
+    Folder(u64),
     File,
 }
 
 struct Content {
     content_type: ContentType,
-    parent: Option<u32>,
-    children: Option<Vec<u32>>,
+    parent: Option<u64>,
+    children: Option<Vec<u64>>,
     size: Option<u32>, // if folder, sum of sizes below. if file, size of file
 }
 
 struct FolderStructure {
-    map: HashMap<u32, Content>,
+    map: HashMap<u64, Content>,
 }
 
-fn generate_uid() -> u32 {
-    0
+fn get_hash(fully_qualified_path: &str) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    fully_qualified_path.hash(&mut hasher);
+    hasher.finish()
 }
 impl FolderStructure {
     pub fn new() -> FolderStructure {
         let mut map = HashMap::new();
         map.insert(
-            0 as u32,
+            0 as u64,
             Content {
                 content_type: ContentType::Folder(0),
                 parent: None,
@@ -35,14 +39,14 @@ impl FolderStructure {
         FolderStructure { map }
     }
 
-    pub fn add_content(&mut self, parent_id: u32, content: Content) {
+    pub fn add_content(&mut self, fully_qualified_path: &str, parent_id: u64, content: Content) {
         let parent = self.map.get_mut(&parent_id).unwrap();
-        let uid = generate_uid();
+        let uid = get_hash(fully_qualified_path);
         parent.children.as_mut().unwrap().push(uid);
         self.map.insert(uid, content);
     }
 
-    fn modify_folders(&mut self, immediate_parent_id: u32, size: u32) {
+    fn modify_folders(&mut self, immediate_parent_id: u64, size: u32) {
         let mut parent_id_opt = Some(immediate_parent_id);
         while let Some(parent_id) = parent_id_opt {
             let mut parent = self.map.get_mut(&parent_id).unwrap();
@@ -51,7 +55,7 @@ impl FolderStructure {
         }
     }
 
-    pub fn add_file(&mut self, parent_id: u32, size: u32) {
+    pub fn add_file(&mut self, fully_qualified_path: &str, parent_id: u64, size: u32) {
         let child = Content {
             content_type: ContentType::File,
             parent: Some(parent_id),
@@ -60,18 +64,19 @@ impl FolderStructure {
         };
 
         self.modify_folders(parent_id, size);
-        self.add_content(parent_id, child);
+        self.add_content(fully_qualified_path, parent_id, child);
     }
 
-    pub fn add_folder(&mut self, parent_id: u32) {
-        let folder_key = generate_uid();
+    pub fn add_folder(&mut self, fully_qualified_path: &str, parent_id: u64) -> u64 {
+        let folder_key = get_hash(fully_qualified_path);
         let child = Content {
             content_type: ContentType::Folder(folder_key),
             parent: Some(parent_id),
             size: None,
             children: Some(Vec::new()),
         };
-        self.add_content(parent_id, child);
+        self.add_content(fully_qualified_path, parent_id, child);
+        folder_key
     }
 }
 
@@ -91,11 +96,17 @@ mod tests {
     #[test]
     fn tree_test() {
         let mut fs = FolderStructure::new();
-        fs.add_file(0, 10);
+        fs.add_file("/", 0, 10);
         let test = fs.map.get(&0).unwrap();
         assert_eq!(test.size, Some(10));
 
-        fs.add_folder(0);
+        let k = fs.add_folder("/test/", 0);
+        fs.add_file("/test/test.log", k, 20);
+
+        let test = fs.map.get(&0).unwrap();
+        assert_eq!(test.size, Some(30));
+        let test2 = fs.map.get(&k).unwrap();
+        assert_eq!(test2.size, Some(20));
     }
 
     #[test]
