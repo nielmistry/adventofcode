fb0ef5caefe3fde5556de67b7cce120e601178f2
Niel Mistry <niel@nielmistry.com>
2022-12-29 10:54:05 -0500

    unsuccesful try with strong refs

--- 2022/rust/day7/src/lib.rs
+++ 2022/rust/day7/src/lib.rs
@@ -1,24 +1,86 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::ops::Deref;
+use std::rc::{Rc, Weak};
 
-struct Node {
-    size: Option<u32>,
-    parent: Option<Rc<RefCell<Node>>>,
-    children: Vec<Rc<RefCell<Node>>>,
+// Reference: https://developerlife.com/2022/02/24/rust-non-binary-tree/#naive-approach-using-weak-and-strong-references
+type NodeDataRef<T> = Rc<NodeData<T>>;
+type WeakNodeDataRef<T> = Weak<NodeData<T>>;
+
+type Parent<T> = RefCell<WeakNodeDataRef<T>>;
+type Children<T> = RefCell<Vec<Child<T>>>;
+type Child<T> = NodeDataRef<T>;
+
+struct NodeData<T> {
+    value: T,
+    parent: Parent<T>,
+    children: Children<T>,
 }
 
-impl Node {
-    pub fn new() -> Node {
-        return Node {
-            size: None,
-            parent: None,
-            children: vec![],
+struct Node<T> {
+    r: NodeDataRef<T>,
+}
+
+impl<T> Deref for Node<T> {
+    type Target = NodeData<T>;
+    fn deref(&self) -> &Self::Target {
+        &self.r
+    }
+}
+
+impl<T> Node<T> {
+    pub fn new(value: T) -> Node<T> {
+        let new_node = NodeData {
+            value,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(Vec::new()),
         };
+        let rc_ref = Rc::new(new_node);
+        Node { r: rc_ref }
+    }
+
+    pub fn get_node_ref_copy(self: &Self) -> NodeDataRef<T> {
+        Rc::clone(&self.r)
+    }
+
+    pub fn create_and_add_child(self: &Self, child_value: T) -> NodeDataRef<T> {
+        let child_node = Node::new(child_value);
+
+        {
+            let mut children = self.children.borrow_mut();
+            children.push(child_node.get_node_ref_copy());
+        }
+        {
+            let mut childs_parent = child_node.parent.borrow_mut();
+            *childs_parent = Rc::downgrade(&self.get_node_ref_copy());
+        }
+
+        child_node.get_node_ref_copy()
+    }
+
+    pub fn get_parent(self: &Self) -> Option<NodeDataRef<T>> {
+        let parent = self.parent.borrow();
+        let node_data = parent.upgrade();
+        if let Some(data) = node_data {
+            Some(data)
+        } else {
+            None
+        }
+    }
+}
+
+struct Tree<T> {
+    root: Node<T>,
+}
+
+impl<T> Tree<T> {
+    pub fn new(value: T) -> Tree<T> {
+        Tree {
+            root: Node::new(value),
+        }
     }
 }
 
 pub fn part1(input: &str) -> usize {
-    let a = Node::new();
     0
 }
 
@@ -29,7 +91,21 @@ pub fn part2(input: &str) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs;
+    use std::{borrow::Borrow, fs};
+
+    #[test]
+    fn tree_test() {
+        let tree: Tree<u32> = Tree::new(0);
+        tree.root.create_and_add_child(10);
+        tree.root.create_and_add_child(20);
+
+        let children = tree.root.children.borrow();
+        assert_eq!(children.len(), 2);
+        let child1 = children.get(0).unwrap();
+        assert_eq!(child1.value, 10);
+
+        let parent = *child1.parent.borrow();
+    }
 
     #[test]
     fn part1_test() {
